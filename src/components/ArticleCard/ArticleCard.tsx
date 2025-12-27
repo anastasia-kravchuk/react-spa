6e33e2c125511fde57b26f29424b276ce3dc3173
Anastasia <ahinata1717@gmail.com>
2025-12-27 16:34:06 +0200

    feat: made an ArticleCard component fully clickable

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -18,35 +18,38 @@ export const ArticleCard: React.FC<Props> = ({ article }) => {
   const truncatedSummary = truncateText(article.summary, 100);
   return (
     <article className="card">
-      <div className="card-image">
-        <img
-          src={article.image_url}
-          className="card-image__img"
-          alt="Article photo"
-        />
-      </div>
-
-      <div className="card-content">
-        <div className="card-date">
-          <CalendarTodayIcon sx={{ fontSize: 14 }} />
-          <p>{formatDate(article.published_at)}</p>
+      <Link
+        to={`/article/${article.id}`}
+        className="card-link"
+        aria-label={`Read article: ${article.title}`}
+      >
+        <div className="card-image">
+          <img
+            src={article.image_url}
+            className="card-image__img"
+            alt="Article photo"
+          />
         </div>
 
-        <h2 className="card-title">{highlightText(article.title, search)}</h2>
+        <div className="card-content">
+          <div className="card-date">
+            <CalendarTodayIcon sx={{ fontSize: 14 }} />
+            <p>{formatDate(article.published_at)}</p>
+          </div>
 
-        <div className="card-summary">
-          {highlightText(truncatedSummary, search)}
+          <h2 className="card-title">{highlightText(article.title, search)}</h2>
+
+          <div className="card-summary">
+            {highlightText(truncatedSummary, search)}
+          </div>
+          <div className="card-button">
+            <span className="button-read-more">
+              Read more
+              <ArrowForwardIcon sx={{ fontWeight: 200 }} />
+            </span>
+          </div>
         </div>
-        <div className="card-button">
-          <Link
-            to={`/article/${article.id}`}
-            className="button-read-more"
-          >
-            Read more
-            <ArrowForwardIcon sx={{ fontWeight: 200 }} />
-          </Link>
-        </div>
-      </div>
+      </Link>
     </article>
   );
 };
