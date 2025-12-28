3d22dd2d8c7281520c9ce823916c966e890e7c52
Anastasia <ahinata1717@gmail.com>
2025-12-28 14:16:10 +0200

    chore: remove unnecessary font rendering styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import { HomePage } from './pages/HomePage/HomePage'
 import { ArticlePage } from './pages/ArticlePage/ArticlePage'
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage'
